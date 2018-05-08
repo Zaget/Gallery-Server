7d5f56f6a7441aded91c23918d38e5fb4daff3ad
Steve Richards <steve.richards@gmail.com>
2018-05-08 11:38:36 -0700

    Update previous tests to work with large dataset

--- tests/DB.test.js
+++ tests/DB.test.js
@@ -2,21 +2,21 @@ const db = require('../database/list.js');
 
 test('the data is an array', () => {
   expect.assertions(1);
-  return db.find().then((data) => {
+  return db.find({ 'place_id': '0' }).then((data) => {
     expect(Array.isArray(data)).toBe(true);
   });
 });
 
-test('the photos data is an array of length 10', () => {
+test('the photos data is an array with a length between 10 and 20', () => {
   expect.assertions(1);
-  return db.findOne({'place_id': 'ChIJFUBxSY6AhYARwOaLV7TsLjw'}).then((data) => {
-    expect(data.photos.length).toBe(10);
+  return db.findOne({ 'place_id': '0' }).then((data) => {
+    expect(data.photos.length >= 10 && data.photos.length <= 20).toBe(true);
   });
 });
 
 test('the data has a place_id', () => {
   expect.assertions(1);
-  return db.findOne({'place_id': 'ChIJFUBxSY6AhYARwOaLV7TsLjw'}).then(data => {
+  return db.findOne({ 'place_id': '0' }).then(data => {
     expect(!!data.place_id).toBe(true);
   });
 });
